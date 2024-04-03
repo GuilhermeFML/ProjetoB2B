7651b00fe93435967f0324529f73feadabb3d24f
GL <a@b.com>
2024-04-03 09:38:23 -0300

    sadsadsad
    Co-authored-by: GabrielCoghi14 <GabrielCoghi14@users.noreply.github.com>

---
+++ assets/Js/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("#tempo").addEventListener("input", calcular);
+    document.querySelectorAll('input[name="prototipo"]').forEach((radio) => {
+        radio.addEventListener("input", calcular);
+    });
+});
+
+function calcular() {
+    const QuantidadeTempo = document.querySelector("#tempo").value;
+    let tempo = QuantidadeTempo * 1;
+    document.querySelector("#valor").innerText = "Meses: " + tempo ;
+
+    let valor = 0;
+    const tipo = document.querySelector("#esporte").value; 
+    if (tipo == 2) valor += 2000;
+
+    const prototipoSim = document.querySelector("#prototipo_sim").checked;
+    const prototipoNao = document.querySelector("#prototipo_nao").checked;
+
+}
+document.getElementById("search").addEventListener("click", function() {
+   
+    alert("Pesquisa realizada");})
+
+    document.getElementById("exp").addEventListener("click", function() {
+   
+        alert("Abrindo categorias...");})
